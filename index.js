44fcc2049d6ffc01b915c347471063a0e7cf1f3a
deosaju <deosaju2260@gmail.com>
2022-05-28 20:53:31 +0530

    Added Script(Peerjs)+ngrok for wide execution

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const { v4: uuidv4 } = require("uuid");
+const twilio = require('twilio');
 
 
 const port = process.env.PORT || 3000;
@@ -79,6 +80,44 @@ io.on("connection", (socket) => {
     socket.to(theroomid).emit("update_canvas", data);
     //   socket.emit('update_canvas',data);
   });
+});
+
+//twilo voice over api
+
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const urlencoded = require('body-parser').urlencoded;
+
+// Update with your own phone number in E.164 format
+const MODERATOR = '+19784643523';
+
+// Parse incoming POST params with Express middleware
+app.use(urlencoded({ extended: false }));
+
+// Create a route that will handle Twilio webhook requests, sent as an
+// HTTP POST to /voice in our application
+app.post('/voice', (request, response) => {
+  // Use the Twilio Node.js SDK to build an XML response
+  const twiml = new VoiceResponse();
+
+  // Start with a <Dial> verb
+  const dial = twiml.dial();
+  // If the caller is our MODERATOR, then start the conference when they
+  // join and end the conference when they leave
+  if (request.body.From == MODERATOR) {
+    dial.conference('My conference', {
+      startConferenceOnEnter: true,
+      endConferenceOnExit: true,
+    });
+  } else {
+    // Otherwise have the caller join as a regular participant
+    dial.conference('My conference', {
+      startConferenceOnEnter: false,
+    });
+  }
+
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(twiml.toString());
 });
 
 // Create an HTTP server and listen for requests on port 3000
